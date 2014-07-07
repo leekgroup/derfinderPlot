b9c5029b468c3770b036f691d91d6480dfa5d545
lcolladotor <lcollado@jhsph.edu>
2014-07-07 14:51:56 -0400

    Migrated from derfinder

---
+++ R/zzz.R
@@ -0,0 +1,17 @@
+## Based on https://github.com/hadley/ggplot2/blob/master/R/zzz.r
+.onAttach <- function(...) {
+    if (!interactive() || stats::runif(1) > 0.05) return()
+        
+    tips <- c(
+        "Found a bug? Report it at https://github.com/lcolladotor/derfinderPlot/issues",
+        "Want to contribute a new feature? Fork derfinderPlot at\n https://github.com/lcolladotor/derfinderPlot/fork\nThen submit a pull request =)",
+        paste("Find out what's changed in derfinder with\n",
+            "news(Version == \"", utils::packageVersion("derfinder"),
+            "\", package = \"derfinder\")", sep = ""),
+        "Create HTML reports from derfinder results using derfinderReport available at\nhttps://github.com/lcolladotor/derfinderReport",
+        "Use suppressPackageStartupMessages to eliminate package startup messages."
+    )
+    
+    tip <- sample(tips, 1, prob = c(0.4, 0.2, 0.2, 0.1, 0.1))
+    packageStartupMessage(tip)
+}
