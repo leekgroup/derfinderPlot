b5b61b697baf582343cd82fa666ca6135f8f5ab4
l.collado-torres <l.collado-torres@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-03-13 21:18:57 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: 6e760857669786ec11a1b79090743bc913706650
    
        Show less startup messages
    
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/derfinderPlot@100666 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,9 @@
 ## Based on https://github.com/hadley/ggplot2/blob/master/R/zzz.r
 .onAttach <- function(...) {
-    if (!interactive() || stats::runif(1) > 0.05) return()
+    if (!interactive() || stats::runif(1) > 0.95) return()
         
     tips <- c(
-        "Found a bug? Report it at https://github.com/lcolladotor/derfinderPlot/issues",
-        "Want to contribute a new feature? Fork derfinderPlot at\n https://github.com/lcolladotor/derfinderPlot/fork\nThen submit a pull request =)",
+        "Want to contribute a new feature? Fork derfinderPlot at\n https://github.com/lcolladotor/derfinderPlot\nThen submit a pull request =)",
         paste("Find out what's changed in derfinderPlot with\n",
             "news(Version == \"", utils::packageVersion("derfinderPlot"),
             "\", package = \"derfinderPlot\")", sep = ""),
@@ -12,6 +11,6 @@
         "Use suppressPackageStartupMessages to eliminate package startup messages."
     )
     
-    tip <- sample(tips, 1, prob = c(0.4, 0.2, 0.2, 0.1, 0.1))
+    tip <- sample(tips, 1, prob = c(0.2, 0.6, 0.1, 0.1))
     packageStartupMessage(tip)
 }
