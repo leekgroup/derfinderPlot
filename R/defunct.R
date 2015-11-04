164f2e0781bcfee8ff6b10b94553f45f9b56e2bf
Leonardo Collado-Torres <lcollado@jhu.edu>
2015-11-04 18:16:36 +0000

    v1.5.3 -- Changed depracated functions from previous cycle to defunct
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/derfinderPlot@110329 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/defunct.R
@@ -0,0 +1,26 @@
+#' Defunct functions in package \sQuote{derfinderPlot}
+#'
+#' These functions are defunct and no longer available.
+#'
+#' @details 
+#'
+#' The following functions are defunct; use the replacement indicated below:
+#'  \itemize{
+#'
+#'    \item{plot_overview: \code{\link{plotOverview}}}
+#'    \item{plot_cluster: \code{\link{plotCluster}}}
+#'    \item{plot_region_coverage: \code{\link{plotRegionCoverage}}}
+#'
+#'  }
+#'
+#' @name derfinderPlot-defunct
+#' @export
+plot_overview <- function() { .Defunct('plotOverview')}
+
+#' @rdname derfinderPlot-defunct
+#' @export
+plot_cluster <- function() { .Defunct('plotCluster')}
+
+#' @rdname derfinderPlot-defunct
+#' @export
+plot_region_coverage <- function() { .Defunct('plotRegionCoverage')}
