5beffdeb17cd12d749a6022ae64c0f28d416d7b2
lcolladotor <lcollado@jhsph.edu>
2016-02-20 15:55:35 -0500

    Kill a space typo

--- R/plotRegionCoverage.R
+++ R/plotRegionCoverage.R
@@ -207,7 +207,7 @@ plotRegionCoverage <- function(regions, regionCoverage, groupInfo,
                 nearestAnnotation$distance[i], 'bp from tss:',
                 nearestAnnotation$region[i]), outer = TRUE, cex = 1.3)
         } else {
-            mtext(paste0(nearestAnnotation$name[i], ', nearest tss : ',
+            mtext(paste0(nearestAnnotation$name[i], ', nearest tss: ',
                 nearestAnnotation$region[i]), outer = TRUE, cex = 1.3)
         }
         
